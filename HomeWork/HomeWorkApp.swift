dc2f10228c742e17b031ddaa61c961005dd167db
semsethy <sethyrisk@gmail.com>
2025-08-02 16:24:21 +0700

    Initial Commit

---
+++ HomeWork/HomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkApp.swift
+//  HomeWork
+//
+//  Created by JoshipTy on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
